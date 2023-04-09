99c70ada5a4bb07d8c4e73969cd9562e05e6ad00
Kevin Roney <karoney@dmacc.edu>
2023-04-08 20:04:36 -0500

    Updated import.sql, now successfully initializing dummy data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,20 +1,19 @@
-INSERT INTO volunteers (first_name, last_name, street, city, state, zip, phone, email)
-VALUES ("Diana", "Prince", "123 Themyscira Street", "Paradise Island", "IA", "52345", "5152224433", "wonderWomen@fakeEmail.edu");
+INSERT INTO volunteers (first_name, last_name, street, city, state, zip, phone, email) VALUES ("Diana", "Prince", "123 Themyscira Street", "Paradise Island", "IA", "52345", "5152224433", "wonderWomen@fakeEmail.edu");
 
-INSERT INTO volunteers (first_name, last_name, street, city, state, zip, phone, email)
-VALUES ("Bruce", "Wayne", "123 Main Street", "Gotham City", "IA", "54345", "5153334433", "IamBatMan@fakeEmail.edu");
+INSERT INTO volunteers (first_name, last_name, street, city, state, zip, phone, email) VALUES ("Bruce", "Wayne", "123 Main Street", "Gotham City", "IA", "54345", "5153334433", "IamBatMan@fakeEmail.edu");
 
-INSERT INTO pets (pet_name, pet_type, breed, gender, foster_owner_volunteer_id)
-VALUES ("Voss", "dog", "German Shepherd", "male", null);
+INSERT INTO pets (pet_name, pet_type, breed, gender, foster_owner_volunteer_id) VALUES ("Voss", "dog", "German Shepherd", "male", null);
 
-INSERT INTO pets (pet_name, pet_type, breed, gender, foster_owner_volunteer_id)
-VALUES ("Sasha", "dog", "Alaskan Malamute", "female", null);
+INSERT INTO pets (pet_name, pet_type, breed, gender, foster_owner_volunteer_id) VALUES ("Sasha", "dog", "Alaskan Malamute", "female", null);
 
-INSERT INTO pets (pet_name, pet_type, breed, gender, foster_owner_volunteer_id)
-VALUES ("Pumpkin", "cat", "American Shorthair", "female", null);
+INSERT INTO pets (pet_name, pet_type, breed, gender, foster_owner_volunteer_id) VALUES ("Pumpkin", "cat", "American Shorthair", "female", null);
 
-INSERT INTO pets (pet_name, pet_type, breed, gender, foster_owner_volunteer_id)
-VALUES ("Josslyn", "cat", "Maine Coon", "female", null);
+INSERT INTO pets (pet_name, pet_type, breed, gender, foster_owner_volunteer_id) VALUES ("Josslyn", "cat", "Maine Coon", "female", null);
 
-INSERT INTO pets (pet_name, pet_type, breed, gender, foster_owner_volunteer_id)
-VALUES ("Tweetie", "bird", "Parakeet", "female", null);
+INSERT INTO pets (pet_name, pet_type, breed, gender, foster_owner_volunteer_id) VALUES ("Tweetie", "bird", "Parakeet", "female", null);
+
+INSERT INTO pets (breed, gender, pet_name, pet_type) VALUES ('Husky', 'Femaile', 'Haven', 'Dog'), ('German Shepherd', 'Male', 'Havek', 'Dog'), ('Husky', 'Female', 'Zuli', 'Dog');
+
+INSERT INTO volunteers (city, email, first_name, last_name, phone, state, street, zip) VALUES ('Newton', 'karoney@dmacc.edu', 'Kevin', 'Roney', '641-123-4567', 'Iowa', '123 Main St', '50208'), ('Des Moines', 'JohnSmith@dmacc.edu', 'John', 'Smith', '515-222-4444', 'Iowa', '500 Maple St', '50304');
+
+INSERT INTO customers (city, email, first_name, last_name, phone, state, street, zip) VALUES ('Ankeny', 'LauraDanley@dmacc.edu', 'Laura', 'Danley', '515-626-2468', 'Iowa', '1414 Quaker Drive', '50312'), ('Altoona', 'SherylBarlet@dmacc.edu', 'Sheryl', 'Barlet', '515-555-0123', 'Iowa', '4242 Made Up Street', '54321');
